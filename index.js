155024555b86c6e8fb45a45a46e39a9eebbe3c1c
Grace Nduta <ndutak5674@gmail.com>
2025-03-03 23:27:51 +0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+//Define current user and add a string
+const currentUser="Jason Jojo"
+//Define welcomeMessage to include'welcome to Flat book,'and should include the value of the current user
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
+
+//Define excitedWelcomeMessage to include "WELCOME TO FLATBOOK, " and the value of the current user ending with an exclamation"
+const excitedWelcomeMessage=`WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+//Define shortGreeting to include "welcome" and the first initial of the currentUser ending with an exclamation
+const shortGreeting=`Welcome, ${currentUser[0]}!`
+
+
+
+
+
